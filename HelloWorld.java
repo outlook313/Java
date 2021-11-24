6ea62efb98ea58396b351b518d860b7d0ce02c3b
Habeeb Rahmani <89722385+outlook313@users.noreply.github.com>
2021-11-24 14:01:15 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// A program to display the message
+// "Hello World!" on standard output
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Hello World!");
+}
+}
+// end of class HelloWorld
